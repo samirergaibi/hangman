b11629d4ba9e4ca981d2c8459e00085ca7e69457
Samir Ergaibi <samirergaibi@gmail.com>
2019-02-28 21:10:22 +0100

    completed error handling after player names

--- scripts/script.js
+++ scripts/script.js
@@ -11,17 +11,36 @@ const firstStage = document.getElementById("first-stage");
 const secondStage = document.getElementById("second-stage");
 const playerMsg = document.getElementById("player-msg")
 const executionerMsg = document.getElementById("executioner-msg");
+const errorMsg = document.getElementById("error-msg");
 
 nextBtn.addEventListener("click", function(){
-    hangmanHeading.innerText = "HÄNGA " + playerName.value.toUpperCase();
-    gameExplanation.style.display = "none";
-    firstStage.style.display = "none";
-    secondStage.style.display = "block";
-    playerMsg.innerHTML = "<b>" + capitalizeFirstLetter(playerName.value) + "</b>" + " var god och blunda eller titta bort!";
-    executionerMsg.innerHTML = "<b>" + capitalizeFirstLetter(executionerName.value) + "</b>" + " var god och välj ett ord!";
+    if(checkStringContent(executionerName.value) || checkStringContent(playerName.value)){
+        errorMsg.innerText = "Namnen får bara innehålla bokstäver!";
+    }
+    else if(!executionerName.value || !playerName.value){
+        errorMsg.innerText = "Du måste fylla i namn för spelarna!";
+    }
+    else{
+        errorMsg.style.display = "none";
+        hangmanHeading.innerText = "HÄNGA " + playerName.value.toUpperCase();
+        gameExplanation.style.display = "none";
+        firstStage.style.display = "none";
+        secondStage.style.display = "block";
+        playerMsg.innerHTML = "<b>" + capitalizeFirstLetter(playerName.value) + "</b>" + " var god och blunda eller titta bort!";
+        executionerMsg.innerHTML = "<b>" + capitalizeFirstLetter(executionerName.value) + "</b>" + " var god och välj ett ord!";
+    }
 }, false)
 
-
+// Checks if the string contains and invalid symbols, if it does it returns true
+function checkStringContent(str){
+    const regex = /[^a-zåäöA-ZÅÄÖ]/g;
+    if(regex.test(str)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
 function capitalizeFirstLetter(word){
     return word.charAt(0).toUpperCase() + word.slice(1);
