9e0c948248921a0aeb4daa5d24b498072042fd0a
Samir Ergaibi <samirergaibi@gmail.com>
2019-03-04 14:49:09 +0100

    all logic nearly done

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,6 @@
 
 
 // --References--
-// index.html
 const gameExplanation = document.getElementById("game-explanation");
 const nextBtn = document.getElementById("next-btn");
 const hangmanHeading = document.getElementById("hangman-heading");
@@ -14,14 +13,17 @@ const executionerMsg = document.getElementById("executioner-msg");
 const errorMsg = document.getElementById("error-msg");
 
 nextBtn.addEventListener("click", function(){
+    // checks so that the names only contain letters a-z +åäö
     if(checkStringContent(executionerName.value) || checkStringContent(playerName.value)){
+        errorMsg.style.visibility = "visible";
         errorMsg.innerText = "Namnen får bara innehålla bokstäver!";
     }
     else if(!executionerName.value || !playerName.value){
+        errorMsg.style.visibility = "visible";
         errorMsg.innerText = "Du måste fylla i namn för spelarna!";
     }
     else{
-        errorMsg.style.display = "none";
+        errorMsg.style.visibility = "hidden";
         hangmanHeading.innerText = "HÄNGA " + playerName.value.toUpperCase();
         gameExplanation.style.display = "none";
         firstStage.style.display = "none";
@@ -30,6 +32,32 @@ nextBtn.addEventListener("click", function(){
         executionerMsg.innerHTML = "<b>" + capitalizeFirstLetter(executionerName.value) + "</b>" + " var god och välj ett ord!";
     }
 }, false)
+
+
+// --References--
+const startBtn = document.getElementById("start-btn");
+const word = document.getElementById("word");
+
+startBtn.addEventListener("click", function(){
+    // Checks so that the word only contains characters a-z+åäö
+    if(checkStringContent(word.value)){
+        errorMsg.style.visibility = "visible";
+        errorMsg.innerText = "Du får endast använda bokstäver a-ö i ordet.";
+    }
+    else if(!word.value){
+        errorMsg.style.visibility = "visible";
+        errorMsg.innerText = "Du måste skriva in ett ord.";
+    }
+    else{
+        localStorage.setItem("gameWord", word.value.toUpperCase());
+        localStorage.setItem("executionerName", executionerName.value);
+        localStorage.setItem("playerName", playerName.value);
+        window.location = "game.html";
+    }
+}, false)
+
+
+
 
 // Checks if the string contains and invalid symbols, if it does it returns true
 function checkStringContent(str){
